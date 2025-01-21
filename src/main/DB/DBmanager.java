bd9ec55a4fd18238c1630745d525afde04754200
Maksim Shcherbakov <94864283+M4RFF@users.noreply.github.com>
2025-01-21 11:48:18 +0100

    [HW8] Implement Configuration File for Database Properties

--- src/main/DB/DBmanager.java
+++ src/main/DB/DBmanager.java
@@ -1,19 +1,39 @@
 package src.main.DB;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 
 // This class is created for managing connections
 public class DBmanager {
 
-    private static final String URL = "jdbc:postgresql://localhost:5432/coworking_space";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "max123";
+    private static final String PROP_FILE = "src/main/DB/db.properties";
+
+    private static String URL;
+    private static String USERNAME;
+    private static String PASSWORD;
+
+    static {
+        try {
+            Properties prop = new Properties();
+            prop.load(new FileInputStream(PROP_FILE));
+
+            URL = prop.getProperty("db.url");
+            USERNAME = prop.getProperty("db.username");
+            PASSWORD = prop.getProperty("db.password");
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Can't load db properties!", e);
+        }
+    }
+
 
     public static Connection connect() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
     }
 
 }
